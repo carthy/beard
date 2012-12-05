ec78220c62b0dadb21805af716e4942a043b90ee
meh <meh@schizofreni.co>
2012-12-05 01:57:13 +0100

    test: add some string tests

--- test/string.c
+++ test/string.c
@@ -16,35 +16,41 @@
  * along with libtor. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <tinytest.h>
-#include <tinytest_macros.h>
+#include <private/String.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <time.h>
-
-#include <private/common.h>
-
-#include "value.c"
-#include "free_list.c"
-#include "gc.c"
-#include "integer.c"
-#include "string.c"
-
-struct testgroup_t groups[] = {
-	{ "value/", value_tests },
-	{ "free_list/", free_list_tests },
-	{ "gc/", gc_tests },
-	{ "integer/", integer_tests },
-	{ "string/", string_tests },
-
-	END_OF_GROUPS
-};
-
-int
-main (int argc, const char* argv[])
+void
+test_string_new (void* data)
 {
-	return tinytest_main(argc, argv, groups);
+	GC* gc      = GC_new();
+	String* str = String_new(gc);
+
+end:
+	GC_destroy(gc);
 }
+
+void
+test_string_set_cstr (void* data)
+{
+	GC* gc       = GC_new();
+	String* str  = String_set_cstr(String_new(gc), "æ");
+	String* str2 = String_set_cstr_with_encoding(String_new(gc), "æ", ENCODING_UTF8);
+
+	tt_assert(str->length == 2);
+	tt_assert(str->bytes == 2);
+
+	tt_assert(str2->length == 1);
+	tt_assert(str2->bytes == 2);
+
+end:
+	String_destroy(str);
+	String_destroy(str2);
+
+	GC_destroy(gc);
+}
+
+struct testcase_t string_tests[] = {
+	{ "new", test_string_new },
+	{ "set_cstr", test_string_set_cstr },
+
+	END_OF_TESTCASES
+};
