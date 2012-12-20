67246d7dc1f2c00c04ed274cbc5401f9f4c02969
meh <meh@schizofreni.co>
2012-12-20 03:30:27 +0100

    test/map: adapt to new implementation

--- test/map.c
+++ test/map.c
@@ -21,39 +21,78 @@
 void
 test_map_new (void* data)
 {
-	Map* vec = Map_new(runtime);
+	Map* map = Map_new(runtime);
 
-	tt_assert(vec);
+	tt_assert(map);
 
 end:
-	Map_destroy(vec);
+	Map_destroy(map);
 }
 
 void
-test_map_set (void* data)
+test_map_put (void* data)
 {
-	Map* vec = Map_new(runtime);
-	Map_put(vec, 0, TRUE);
+	Map*   map  = Map_new(runtime);
+	Tuple* pair = Map_put(map, hash_for(TRUE), TRUE, FALSE);
+
+	tt_assert(is_true(Tuple_get(pair, 0)));
+	tt_assert(is_false(Tuple_get(pair, 1)));
+
+	pair = Map_put(map, hash_for(TRUE), TRUE, NIL);
+
+	tt_assert(is_true(Tuple_get(pair, 0)));
+	tt_assert(is_nil(Tuple_get(pair, 1)));
 
 end:
-	Map_destroy(vec);
+	Map_destroy(map);
 }
 
 void
 test_map_get (void* data)
 {
-	Map* vec = Map_new(runtime);
-	Map_put(vec, 0, TRUE);
-	tt_assert(is_boolean(Map_get(vec, 0)));
+	Map*   map  = Map_new(runtime);
+	Tuple* pair = Map_put(map, hash_for(TRUE), TRUE, FALSE);
+
+	tt_assert(is_true(Tuple_get(pair, 0)));
+	tt_assert(is_false(Tuple_get(pair, 1)));
+
+	pair = Map_put(map, hash_for(TRUE), TRUE, NIL);
+
+	tt_assert(is_true(Tuple_get(pair, 0)));
+	tt_assert(is_nil(Tuple_get(pair, 1)));
+
+	tt_assert(is_true(Map_get_key(map, hash_for(TRUE))));
+	tt_assert(is_nil(Map_get_value(map, hash_for(TRUE))));
 
 end:
-	Map_destroy(vec);
+	Map_destroy(map);
+}
+
+void
+test_map_delete (void* data)
+{
+	Map*   map  = Map_new(runtime);
+	Tuple* pair = Map_put(map, hash_for(TRUE), TRUE, FALSE);
+
+	tt_assert(is_true(Tuple_get(pair, 0)));
+	tt_assert(is_false(Tuple_get(pair, 1)));
+
+	pair = Map_delete(map, hash_for(TRUE));
+
+	tt_assert(is_true(Tuple_get(pair, 0)));
+	tt_assert(is_false(Tuple_get(pair, 1)));
+
+	tt_assert(!Map_has(map, hash_for(TRUE)));
+
+end:
+	Map_destroy(map);
 }
 
 struct testcase_t map_tests[] = {
 	{ "new", test_map_new },
-	{ "set", test_map_set },
+	{ "put", test_map_put },
 	{ "get", test_map_get },
+	{ "delete", test_map_delete },
 
 	END_OF_TESTCASES
 };
