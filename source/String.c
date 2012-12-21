13c8d1690f65e579ff5a3b90afd9fdadc507bcf7
meh <meh@schizofreni.co>
2012-12-22 00:03:08 +0100

    String/hash: take into consideration the value type

--- source/String.c
+++ source/String.c
@@ -309,7 +309,7 @@ String_hash (String* self)
 		encoding = 0;
 	}
 
-	CACHE(self)->hash = SIPHASH(RUNTIME_FOR(self), self->buffer, self->bytes) ^ encoding;
+	CACHE(self)->hash = SIPHASH(RUNTIME_FOR(self), self->buffer, self->bytes) ^ ((VALUE_TYPE_STRING << 4) ^ encoding);
 
 	return CACHE(self)->hash;
 }
